d6d3988b13de7ee2f0c5f2072ead31af392b9585
fatbob313 <martin@fatbob.nu>
2010-02-13 01:29:53 +0000

    Add missing includes

--- src/channel.c
+++ src/channel.c
@@ -29,6 +29,8 @@
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include <limits.h>
+#include <stdlib.h>
+#include <string.h>
 #include "log.h"
 #include "list.h"
 #include "client.h"
