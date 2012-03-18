2038e35a66ae00859799886f8e5258649b726088
Martin Johansson <martin@fatbob.nu>
2012-03-18 20:21:13 +0100

    Update to version 0.2.10

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.9"
-#define UMURMUR_CODENAME "Batshit Crazy"
+#define UMURMUR_VERSION "0.2.10"
+#define UMURMUR_CODENAME "Bats in the Belfry"
 
 #endif
 
