bbd9b6d4378b7109929f6d9626310020c77ae230
Martin Johansson <martin@fatbob.nu>
2012-11-07 22:51:15 +0100

    Prepare for next protocol version 1.2.4.

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.10"
-#define UMURMUR_CODENAME "Bats in the Belfry"
+#define UMURMUR_VERSION "0.2.11"
+#define UMURMUR_CODENAME "Demented"
 
 #endif
 
