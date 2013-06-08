a80a085904324c437341acb00df7c8be6b72936d
Martin Johansson <martin@fatbob.nu>
2013-06-08 22:57:56 +0200

    Update to 0.2.13

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.12"
-#define UMURMUR_CODENAME "Disturbed"
+#define UMURMUR_VERSION "0.2.13"
+#define UMURMUR_CODENAME "Deranged"
 
 #endif
 
