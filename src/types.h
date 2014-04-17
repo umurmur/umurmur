7470834ac6613c2e22626f9511384f025f16768e
Felix Morgner <felix.morgner@gmail.com>
2014-04-17 08:10:18 +0200

    Use c99 standard bool when available

--- src/types.h
+++ src/types.h
@@ -1,10 +1,19 @@
 #ifndef TYPES_H_90878954
 #define TYPES_H_90878954
 
+#if __STDC_VERSION__ == 199901L
+
+#include <stdbool.h>
+typedef bool bool_t;
+
+#else // __STDC_VERSION__
+
 typedef enum {
 	false,
 	true
 } bool_t;
+
+#endif // __STDC_VERSION
 
 #endif
 
