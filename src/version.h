906b0a21bdd1e2c256090ca57569c63705dabbf2
Felix Morgner <felix.morgner@gmail.com>
2015-04-07 12:01:12 +0200

    Release 0.2.16

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.15"
-#define UMURMUR_CODENAME "Drool"
+#define UMURMUR_VERSION "0.2.16"
+#define UMURMUR_CODENAME "Agrajag"
 
 #endif
 
