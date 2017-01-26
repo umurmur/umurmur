9fa95cb052994369aefd2f10951c1dac2f510ee3
Felix Morgner <felix.morgner@gmail.com>
2017-01-26 14:06:08 +0100

    Support for mbed TLS 2.4 fixes #102

--- src/ssl.h
+++ src/ssl.h
@@ -90,7 +90,11 @@ typedef	ssl_context SSL_handle_t;
 
 #elif defined(USE_MBEDTLS)
 #include <mbedtls/ssl.h>
+#if (MBEDTLS_VERSION_MINOR > 3)
+#include <mbedtls/net_socket.h>
+#else
 #include <mbedtls/net.h>
+#endif
 #include <mbedtls/version.h>
 
 #if defined(MBEDTLS_VERSION_MAJOR)
