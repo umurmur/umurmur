500668529625290605ab7afe0d62cd507ee25a99
Felix Morgner <felix.morgner@gmail.com>
2017-02-06 09:38:04 +0100

    Bump to 0.2.17rc1

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.16a"
-#define UMURMUR_CODENAME "Agrajag"
+#define UMURMUR_VERSION "0.2.17rc1"
+#define UMURMUR_CODENAME "Colin"
 
 #endif
 
