09eade68da6588e8ab029a4a06ad00ed77a250c0
Vitaly Sinilin <vs@kp4.ru>
2018-03-04 14:34:13 +0300

    Get rid of unnecessary code duplication
    Since 571e132 switch_user() happens later so the same code path
    is applicable to both daemon and foreground modes.

--- src/main.c
+++ src/main.c
@@ -322,23 +322,14 @@ int main(int argc, char **argv)
 			Log_init(false);
 			if (pidfile != NULL)
 				lockfile(pidfile);
-#ifdef POSIX_PRIORITY_SCHEDULING
-			/* Set the scheduling policy, has to be called after daemonizing
-			 * but before we drop privileges */
-			if (realtime)
-				setscheduler();
-#endif
-
 		}
 		else Log_init(true);
 
 #ifdef POSIX_PRIORITY_SCHEDULING
-		/* We still want to set scheduling policy if nodaemon is specified,
-		 * but if we are daemonizing setscheduler() will be called above */
-		if (nodaemon) {
-			if (realtime)
-				setscheduler();
-		}
+		/* Set the scheduling policy, has to be called after daemonizing
+		 * but before we drop privileges */
+		if (realtime)
+			setscheduler();
 #endif
 
 		signal(SIGCHLD, SIG_IGN); /* ignore child */
