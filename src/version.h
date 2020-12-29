0a1de88dcbc752d640e0385e8e2c6a3af0c6258c
Martin Johansson <martin@fatbob.nu>
2020-12-29 21:30:01 +0100

    Update version to 0.2.18

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.17"
-#define UMURMUR_CODENAME "Colin"
+#define UMURMUR_VERSION "0.2.18"
+#define UMURMUR_CODENAME "Meshuga"
 
 #endif
 
