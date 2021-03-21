19110397d8e79752ef8c4ef9787f13faf3effc2a
Martin Johansson <martin@fatbob.nu>
2021-03-21 21:49:04 +0100

    Update to version 0.2.20

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.19"
-#define UMURMUR_CODENAME "Wacko"
+#define UMURMUR_VERSION "0.2.20"
+#define UMURMUR_CODENAME "Bonkers"
 
 #endif
 
