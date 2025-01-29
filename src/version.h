b4c24ff8cd2c2a7a818e75331e33a97b6995ca8f
Troy C. <327529+troxor@users.noreply.github.com>
2025-01-28 22:15:15 -0600

    Update to version 0.3.0

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_989876
 #define VERSION_H_989876
 
-#define UMURMUR_VERSION "0.2.20"
-#define UMURMUR_CODENAME "Bonkers"
+#define UMURMUR_VERSION "0.3.0"
+#define UMURMUR_CODENAME "Loopy"
 
 #endif
 
