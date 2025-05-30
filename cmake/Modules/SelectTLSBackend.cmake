f6b8b881e7e1ab7d5e520d3d32810656d31d050c
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2025-05-30 12:53:41 +0300

    Update SelectTLSBackend.cmake
    Use IMPORTED targets when available, simplify logic. Remove conditionals for *_FOUND checking as find_package(... REQUIRED) will fail on unmet dependencies.

--- cmake/Modules/SelectTLSBackend.cmake
+++ cmake/Modules/SelectTLSBackend.cmake
@@ -10,21 +10,17 @@ function(SelectTLSBackend SSL)
 
   if("${SSL}" STREQUAL "openssl")
     find_package(OpenSSL REQUIRED)
-    set(SSL_VERSION "OpenSSL ${_OPENSSL_VERSION}")
+    set(SSL_VERSION "OpenSSL ${OPENSSL_VERSION}")
 
-    if(OPENSSL_FOUND)
-      set(LIBRARIES ${OPENSSL_LIBRARIES})
-      set(INCLUDE_DIR ${OPENSSL_INCLUDE_DIR})
-      set(LIBRARY_DIR ${OPENSSL_LIB_DIR})
+    set(LIBRARIES OpenSSL::SSL)
 
-      if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-deprecated-declarations" PARENT_SCOPE)
-      endif()
+    if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-deprecated-declarations" PARENT_SCOPE)
     endif()
 
   elseif("${SSL}" STREQUAL "mbedtls")
     find_package(mbedTLS REQUIRED)
-    set(SSL_VERSION "Mbed TLS ${MBEDTLS_VERSION}")
+    set(SSL_VERSION "MbedTLS ${MbedTLS_VERSION}")
 
     if(MBEDTLS_FOUND)
       set(USE_MBEDTLS ON PARENT_SCOPE)
@@ -36,27 +32,20 @@ function(SelectTLSBackend SSL)
 
   elseif("${SSL}" STREQUAL "gnutls")
     find_package(GnuTLS 3 REQUIRED)
-    set(SSL_VERSION "GnuTLS ${GNUTLS_VERSION}")
-
-    if(GNUTLS_FOUND)
-      set(USE_GNUTLS ON PARENT_SCOPE)
-
-      set(LIBRARIES ${GNUTLS_LIBRARIES})
-      set(INCLUDE_DIR ${GNUTLS_INCLUDE_DIR})
-      set(LIBRARY_DIR ${GNUTLS_LIB_DIR})
-    endif()
-
     # Nettle is the primary and required crypto library for GnuTLS
     find_package(Nettle REQUIRED)
 
-    if(NETTLE_FOUND)
-      list(APPEND LIBRARIES ${NETTLE_LIBRARIES})
-    endif()
+    set(SSL_VERSION "GnuTLS ${GNUTLS_VERSION}")
+
+    set(USE_GNUTLS ON PARENT_SCOPE)
+    set(LIBRARIES GnuTLS::GnuTLS ${NETTLE_LIBRARIES})
 
   endif()
 
   set(SSLIMP_LIBRARIES ${LIBRARIES} PARENT_SCOPE)
   set(SSLIMP_LIBRARY_DIR ${LIBRARY_DIR} PARENT_SCOPE)
   set(SSLIMP_INCLUDE_DIR ${INCLUDE_DIR} PARENT_SCOPE)
+
+  message(STATUS "Using ${SSL_VERSION} as SSL backend")
 
 endfunction()
