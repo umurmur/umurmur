7ef7409afb61e516185f33625dc8a4a6e9f51420
Andrew Kloet <andrew@kloet.net>
2026-08-11 11:57:40 -0400

    feat: Reload certificate on SIGHUP
    On SIGHUP, open the certificate and apply it to the existing context.
    If the certificate doesn't match the private key: abort the reload.
    
    This required the following changes:
    * Make the certificate world readable so the dropped
      privilege user can read it later in life cycle.
    * Simplify OpenBSD pledge rules since the daemon will
      always implicitly need read access for certificate.

--- src/ssl.h
+++ src/ssl.h
@@ -108,6 +108,7 @@ typedef SSL SSL_handle_t;
 
 void SSLi_init(void);
 void SSLi_deinit(void);
+void SSLi_reload_cert(void);
 SSL_handle_t *SSLi_newconnection(int *fd, bool_t *SSLready);
 bool_t SSLi_getSHA1Hash(SSL_handle_t *ssl, uint8_t *hash);
 void SSLi_closeconnection(SSL_handle_t *ssl);
